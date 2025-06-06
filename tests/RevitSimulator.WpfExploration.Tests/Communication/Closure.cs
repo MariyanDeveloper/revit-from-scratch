6ede637246efd9f1a50cec819f74eb2b7dd9d3fa
Mariyan Slesar <118279173+MariyanDeveloper@users.noreply.github.com>
2025-06-06 19:56:31 +0300

    feat: add simple communication examples (#5)
    * feat: add naive communication via seperate dependency
    
    * refactor: rename to CommunicationViaSeparateDepedendency.cs
    
    * feat: add simple callback communication example
    
    * refactor: improve progress message
    
    * feat: add a simple example with closure
    
    * refactor: remove redundant comment

---
+++ tests/RevitSimulator.WpfExploration.Tests/Communication/Closure.cs
@@ -0,0 +1,92 @@
+﻿using Shouldly;
+
+// ReSharper disable once CheckNamespace
+namespace RevitSimulator.WpfExploration.Tests.Communication.Closure;
+
+public record Room(double Area);
+
+public delegate void OnRoomAreaComputed(Room room, double factor, double area);
+
+public class TheDependency
+{
+    public double ComputeAreas(
+        IEnumerable<Room> rooms,
+        OnRoomAreaComputed? onRoomAreaComputedCallback
+    )
+    {
+        var businessLogicFactor = 1.2;
+        return rooms.Sum(room =>
+        {
+            var area = room.Area * businessLogicFactor;
+            onRoomAreaComputedCallback?.Invoke(room, businessLogicFactor, area);
+            return area;
+        });
+    }
+}
+
+public record Progress(string Action, double PercentageDone)
+{
+    public static Progress Initial(string action) => new Progress(action, 0);
+
+    public static Progress FromCompletionRatio(
+        string action,
+        double totalCount,
+        double completedCount
+    ) => new Progress(action, completedCount / totalCount * 100);
+};
+
+public class Closure
+{
+    [Fact]
+    public void ShouldTrackProgressUsingClosureGeneratedCallback()
+    {
+        var dependency = new TheDependency();
+        var rooms = new List<Room>() { new Room(10), new Room(15) };
+
+        var progresses = new List<Progress>();
+
+        progresses.Add(Progress.Initial(action: "Beginning area calculation for all rooms"));
+
+        var createOnRoomComputed = (int totalCount) =>
+        {
+            var completedCount = 0;
+            OnRoomAreaComputed onRoomComputed = (room, factor, computedArea) =>
+            {
+                completedCount++;
+                var currentProgress = Progress.FromCompletionRatio(
+                    action: $"Processed room {completedCount}: Area {room.Area} → {computedArea:F2} (factor: {factor})",
+                    totalCount,
+                    completedCount
+                );
+                progresses.Add(currentProgress);
+            };
+
+            return onRoomComputed;
+        };
+
+        var onRoomComputed = createOnRoomComputed(rooms.Count);
+
+        var sum = dependency.ComputeAreas(rooms, onRoomComputed);
+
+        var expectedResult = """
+            [
+              {
+                "action": "Beginning area calculation for all rooms",
+                "percentageDone": 0.0
+              },
+              {
+                "action": "Processed room 1: Area 10 → 12.00 (factor: 1.2)",
+                "percentageDone": 50.0
+              },
+              {
+                "action": "Processed room 2: Area 15 → 18.00 (factor: 1.2)",
+                "percentageDone": 100.0
+              }
+            ]
+            """;
+
+        sum.ShouldBe(30);
+
+        progresses.ShouldMatchInlineSnapshot(expectedResult);
+    }
+}
---
+++ tests/RevitSimulator.WpfExploration.Tests/Communication/CommunicationViaSeparateDepedendency.cs
@@ -0,0 +1,86 @@
+﻿using Shouldly;
+
+namespace RevitSimulator.WpfExploration.Tests.Communication;
+
+public class Events
+{
+    private readonly Dictionary<string, List<Action<object>>> _eventsMap = new();
+
+    public void Subscribe<TArgs>(string name, Action<TArgs> action)
+    {
+        Action<object> castedAction = (obj) => action((TArgs)obj);
+        if (_eventsMap.TryGetValue(name, out var actions))
+        {
+            actions.Add(castedAction);
+            return;
+        }
+        _eventsMap.Add(name, [castedAction]);
+    }
+
+    public void Notify<TArgs>(string name, TArgs args)
+    {
+        if (!_eventsMap.TryGetValue(name, out var actions))
+        {
+            return;
+        }
+        foreach (var action in actions)
+        {
+            action(args);
+        }
+    }
+}
+
+public record NameChanged();
+
+public static class EventKeys
+{
+    public const string OnFirstNameChanged = "OnFirstNameChanged";
+}
+
+public class ComponentA
+{
+    private readonly Events _events;
+    public string SomeValue { get; set; }
+
+    public ComponentA(Events events)
+    {
+        _events = events ?? throw new ArgumentNullException(nameof(events));
+        _events.Subscribe<NameChanged>(
+            EventKeys.OnFirstNameChanged,
+            (args) =>
+            {
+                SomeValue = "Bob";
+            }
+        );
+    }
+}
+
+public class ComponentB
+{
+    private readonly Events _events;
+
+    public ComponentB(Events events)
+    {
+        _events = events ?? throw new ArgumentNullException(nameof(events));
+    }
+
+    public void SomeOperation()
+    {
+        _events.Notify(EventKeys.OnFirstNameChanged, new NameChanged());
+    }
+}
+
+public class CommunicationViaSeparateDepedendency
+{
+    [Fact]
+    public void ShouldShowCommunicationViaSeparateDependency()
+    {
+        var events = new Events();
+        var componentA = new ComponentA(events);
+        var componentB = new ComponentB(events);
+
+        componentB.SomeOperation();
+
+        componentA.SomeValue.ShouldBe("Bob");
+    }
+}
