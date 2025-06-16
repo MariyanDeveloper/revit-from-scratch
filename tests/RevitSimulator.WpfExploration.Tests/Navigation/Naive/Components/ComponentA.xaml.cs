0b59639e3c48fde5ed0d02fa2d2f4deac136f776
Mariyan Slesar <118279173+MariyanDeveloper@users.noreply.github.com>
2025-06-16 21:44:23 +0300

    feat: add naive navigation example (#10)

---
+++ tests/RevitSimulator.WpfExploration.Tests/Navigation/Naive/Components/ComponentA.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace RevitSimulator.WpfExploration.Tests.Navigation.Naive.Components;
+
+public partial class ComponentA : UserControl
+{
+    public ComponentA()
+    {
+        InitializeComponent();
+    }
+}
